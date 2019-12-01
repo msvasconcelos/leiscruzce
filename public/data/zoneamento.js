0c069e56b810b90562cb8f35eb611485db51cbd9
Matheus Vasconcelos <matheus-s.v@hotmail.com>
2019-12-01 18:45:07 -0300

    organizing data

--- public/data/zoneamento.js
+++ public/data/zoneamento.js
@@ -1,30 +1,34 @@
 var zoneamento = [
-{slug:'pwa',
-name:'651',
-data:'22/07/19',
-descricao:'ALTERA A LEI MUNICIPAL N° 649/2019, QUE DEFINE O PERÍMETRO URBANO E DA EXPANSÃO URBANA DO MUNICÍPIO DE CRUZ E DETERMINA OUTRAS PROVIDÊNCIAS.',
-categoria:'ZONEAMENTO',
-exercicio:'2019'
-},
-{slug:'pwa',
-name:'650',
-data:'22/07/19',
-descricao:'ALTERA A LEI MUNICIPAL N° 648/2019, QUE DISPÕE SOBRE A REGULARIZAÇÃO FUNDIÁRIA URBANA NO ÂMBITO DO MUNICÍPIO DE CRUZ, COMPREENDENDO A ZONA RURAL, URBANA E SEDE, NOS TERMOS QUE DO ART. 13, INCISO I C/C ART. 30, I E § 2º DA LEI FEDERAL N° 13.465, DE 11/07/2017 E DO DECRETO FEDERAL N° 9.310/2018, NA FORMA QUE INDICA E DÁ OUTRAS PROVIDÊNCIAS.',
-categoria:'ZONEAMENTO',
-exercicio:'2019'
-},
-{slug:'pwa',
-name:'649',
-data:'28/06/19',
-descricao:'DEFINE O PERÍMETRO URBANO E DA EXPANSÃO URBANA DO MUNICÍPIO DE CRUZ E DETERMINA OUTRAS PROVIDÊNCIAS.',
-categoria:'ZONEAMENTO',
-exercicio:'2019'
-},
-{slug:'pwa',
-name:'10',
-data:'25/08/86',
-descricao:'TORNA O TERRENO EM FRENTE AO SENADIM CLUB DE UTILIDADE PÚBLICA E DÁ OUTRAS PROVIDÊNCIAS.',
-categoria:'ZONEAMENTO',
-exercicio:'1986'
-}
+    {
+        slug: 'lost-in-cyberspace',
+        name: '651',
+        data: '22/07/19',
+        descricao: 'ALTERA A LEI MUNICIPAL N° 649/2019, QUE DEFINE O PERÍMETRO URBANO E DA EXPANSÃO URBANA DO MUNICÍPIO DE CRUZ E DETERMINA OUTRAS PROVIDÊNCIAS.',
+        categoria: 'ZONEAMENTO',
+        exercicio: '2019'
+    },
+    {
+        slug: 'lost-in-cyberspace',
+        name: '650',
+        data: '22/07/19',
+        descricao: 'ALTERA A LEI MUNICIPAL N° 648/2019, QUE DISPÕE SOBRE A REGULARIZAÇÃO FUNDIÁRIA URBANA NO ÂMBITO DO MUNICÍPIO DE CRUZ, COMPREENDENDO A ZONA RURAL, URBANA E SEDE, NOS TERMOS QUE DO ART. 13, INCISO I C/C ART. 30, I E § 2º DA LEI FEDERAL N° 13.465, DE 11/07/2017 E DO DECRETO FEDERAL N° 9.310/2018, NA FORMA QUE INDICA E DÁ OUTRAS PROVIDÊNCIAS.',
+        categoria: 'ZONEAMENTO',
+        exercicio: '2019'
+    },
+    {
+        slug: 'lost-in-cyberspace',
+        name: '649',
+        data: '28/06/19',
+        descricao: 'DEFINE O PERÍMETRO URBANO E DA EXPANSÃO URBANA DO MUNICÍPIO DE CRUZ E DETERMINA OUTRAS PROVIDÊNCIAS.',
+        categoria: 'ZONEAMENTO',
+        exercicio: '2019'
+    },
+    {
+        slug: 'lost-in-cyberspace',
+        name: '10',
+        data: '25/08/86',
+        descricao: 'TORNA O TERRENO EM FRENTE AO SENADIM CLUB DE UTILIDADE PÚBLICA E DÁ OUTRAS PROVIDÊNCIAS.',
+        categoria: 'ZONEAMENTO',
+        exercicio: '1986'
+    }
 ];
